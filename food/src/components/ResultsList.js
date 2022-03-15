e097db3b806968fc7c21fc21a4d96efc7da7161b
Steve <sker078@aucklanduni.ac.nz>
2022-03-15 16:39:46 +1300

    added basic reusable details

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({title, results}) => {
 
@@ -12,7 +13,7 @@ const ResultsList = ({title, results}) => {
                 keyExtractor={(results) => results.id}
                 renderItem={({item}) => {
                     return(
-                        <Text>{item.name}</Text>
+                        <ResultsDetail result={item} />
                     );
                 }}
             />
