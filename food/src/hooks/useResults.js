157ce1a7318da50d86b6ca84045c2deebbd597e9
Steve <sker078@aucklanduni.ac.nz>
2022-03-15 15:59:03 +1300

    extracted reusable functions

---
+++ food/src/hooks/useResults.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (serachTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: serachTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+        }catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    }
+
+    useEffect(() => {searchApi('pasta')}, [])
+
+    return [searchApi, results, errorMessage];
+};
