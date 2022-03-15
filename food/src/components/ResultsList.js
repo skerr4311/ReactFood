d9504dafa2e9815ffc0f3427071c3cd4327bb754
Steve <sker078@aucklanduni.ac.nz>
2022-03-15 16:47:50 +1300

    finished styling on results detail

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -5,7 +5,7 @@ import ResultsDetail from './ResultsDetail';
 const ResultsList = ({title, results}) => {
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <FlatList 
                 horizontal
@@ -24,7 +24,12 @@ const ResultsList = ({title, results}) => {
 const styles = StyleSheet.create({
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5,
+    },
+    container: {
+        marginBottom: 10,
     }
 });
 
