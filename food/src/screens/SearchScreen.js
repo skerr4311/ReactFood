d120f237ac4824757615d0d87db6e84d7ea5883a
Steve <sker078@aucklanduni.ac.nz>
2022-03-15 15:20:16 +1300

    yelp api hooked up

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term,
+                location: 'san jose'
+            }
+        });
+        setResults(response.data.businesses);
+    }
+
     return (
         <View style={styles.background}>
             <SearchBar 
                 term={term} 
-                onTermChange={newTerm => setTerm(newTerm)}
-                onTermSubmitted={() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmitted={searchApi}
             />
-            <Text>{term}</Text>
+            <Text>We have found {results.length} results</Text>
         </View>
     );
 };
