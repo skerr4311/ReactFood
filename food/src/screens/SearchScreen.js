1ac844c61bc073859509dbba42122ceb91e00d48
Steve <sker078@aucklanduni.ac.nz>
2022-03-15 15:49:43 +1300

    first initalization hook

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,12 @@ const SearchScreen = () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async () => {
+    const searchApi = async (serachTerm) => {
         try {
             const response = await yelp.get('/search', {
                 params: {
                     limit: 50,
-                    term,
+                    term: serachTerm,
                     location: 'san jose'
                 }
             });
@@ -23,12 +23,14 @@ const SearchScreen = () => {
         }
     }
 
+    useEffect(() => {searchApi('pasta')}, [])
+
     return (
         <View style={styles.background}>
             <SearchBar 
                 term={term} 
                 onTermChange={setTerm}
-                onTermSubmitted={searchApi}
+                onTermSubmitted={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
