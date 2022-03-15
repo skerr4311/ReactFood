468308101bcd22a0fde66cd3f8338def9653949a
Steve <sker078@aucklanduni.ac.nz>
2022-03-15 14:32:56 +1300

    Using icons

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
 
     return (
-        <View>
+        <View style={styles.background}>
+            <SearchBar />
             <Text>Search Screen</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'white'
+    }
+});
 
 export default SearchScreen;
